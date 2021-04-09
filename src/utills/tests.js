ce132190fb59a01d9cc857a371256ee422d6b3f7
or berman <orber3@gmail.com>
2021-04-09 15:08:16 +0300

    first commit , header test

---
+++ src/utills/tests.js
@@ -0,0 +1,17 @@
+
+
+export const findByTestAtrr =(component, attr) => { 
+    const wrapper = component.find(`[data-test='${attr}']`)
+    return wrapper;
+}
+
+  
+
+export const checkProps = (component , conformingProps) => { 
+    const propsError= checkPropTypes(component.propTypes
+        ,conformingProps 
+        , 'prop' 
+        , component.name )
+        expect(propsError).toBeUndefined()
+
+}
